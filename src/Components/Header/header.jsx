73a24e035c510921f062e5a450a81bfb633ee4c4
Ely correa <hannagonz1997@gmail.com>
2022-08-09 21:07:49 -0500

    hotfix import in header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import Logo from "../../Assets/LogoMdy.jpg";
 import MenuCategories from "../../Components/MenuCategories/menuCategories.jsx";
 import "./header.css";
