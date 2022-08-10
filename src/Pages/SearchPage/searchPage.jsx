b44d80291a0536325d10b0dff81c24fd6397b551
Ely correa <hannagonz1997@gmail.com>
2022-08-10 02:15:00 -0500

    pagination adden in search page

--- src/Pages/SearchPage/searchPage.jsx
+++ src/Pages/SearchPage/searchPage.jsx
@@ -6,28 +6,58 @@ import { apiKey } from "../../Utils/utils";
 function SearchPage() {
   const [movies, setMovies] = useState([]);
   const params = useParams();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  function nextPage(numberPage) {
+    if (currentPage > 0 && currentPage < movies.total_pages) {
+      setCurrentPage(currentPage + numberPage);
+    }
+  }
 
   useEffect(
     function () {
       async function getMovies() {
         const response = await fetch(
-          `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${params.keyWord}&language=es&page=1`
+          `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${params.keyWord}&language=es&page=${currentPage}`
         );
         const data = await response.json();
-        setMovies(data.results);
+        setMovies(data);
       }
 
       getMovies();
     },
-    [params.keyWord]
+    [params.keyWord, currentPage]
   );
 
   return (
-    <section className="movies-cards-container">
-      {movies?.map(function (movie) {
-        return <CardContainer movie={movie} key={movie.id} />;
-      })}
-    </section>
+    <main>
+      <section className="movies-cards-container">
+        {movies?.results?.map(function (movie) {
+          return <CardContainer movie={movie} key={movie.id} />;
+        })}
+      </section>
+      <div className="buttons-home-container">
+        <button
+          className="button-page-previous"
+          onClick={function () {
+            nextPage(-1);
+          }}
+          disabled={currentPage === 1}
+        >
+          <i className="fa-solid fa-angle-left"></i>
+          Anterior
+        </button>
+        <button
+          className="button-page-next"
+          onClick={function () {
+            nextPage(1);
+          }}
+        >
+          Siguiente
+          <i className="fa-solid fa-chevron-right"></i>
+        </button>
+      </div>
+    </main>
   );
 }
 
